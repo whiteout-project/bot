2da4689f55e3fbbe73e3104e0a979fb73b1cf346
Justinfact <justinguse88@gmail.com>
2025-11-30 19:23:10 +0200

    Fix ValueError: invalid literal for int() with base 10: '' by checking if p is empty

--- cogs/bear_trap.py
+++ cogs/bear_trap.py
@@ -300,7 +300,7 @@ class BearTrap(commands.Cog):
 
                         for row in rows:
                             parts = row[0].split('|')
-                            notification_days.update(int(p) for p in parts)
+                            notification_days.update(int(p) for p in parts if p)
 
                         for next_day in range(1, 8):
                             potential_day = now + timedelta(days=next_day)
@@ -568,7 +568,7 @@ class BearTrap(commands.Cog):
 
                             for row in rows:
                                 parts = row[0].split('|')
-                                notification_days.update(int(p) for p in parts)
+                                notification_days.update(int(p) for p in parts if p)
 
                             for next_day in range(1, 8):
                                 potential_day = now + timedelta(days=next_day)
